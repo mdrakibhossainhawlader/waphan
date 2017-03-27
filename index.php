139b16cabc7adfc90c97564ffe9e1cd12c8400d1
pidewap <pidewap@gmail.com>
2017-03-27 18:47:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,5 @@ require 'func.php';
 $YouTube = new YouTube;
 $trending = $YouTube->trending();
 $json = json_decode($trending);
-
-if($json[0][title]){
-foreach($json as $tjson){
-$tid = $tjson[id];
-$ttitle = $tjson[title];
-$tdur = $tjson[duration];
-$tchannel = $tjson[channel];
-$tview = $tjson[view];
-
-echo '
-<a href="/index.php?id='.$tid.'">'.$ttitle.'</a>
-<br>Dur: '.$tdur.'<br>
-Channel: '.$tchannel.'<br>
-View: '.$tview.'<br>
-';
-}
-}
+print_r($json);
 ?>
