0d0f655190c36391de8a08067bafe7761dabf872
pidewap <pidewap@gmail.com>
2017-04-04 12:51:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require 'func.php';
 $YouTube = new YouTube;
 $trending = $YouTube->trending();
 $json = json_decode($trending);
-print_r($json);
 
 foreach ($json as $youtube) {
   $judul=$youtube->title;
