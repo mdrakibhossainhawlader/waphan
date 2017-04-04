5062027a83df0caf2e5834887ef8582a326f328b
pidewap <pidewap@gmail.com>
2017-04-04 12:50:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,37 @@ $json = json_decode($trending);
 print_r($json);
 
 foreach ($json as $youtube) {
-  echo preg_replace('/[^A-Za-z0-9\  ]/', '', $youtube->title);
+  $judul=$youtube->title;
+  $title=preg_replace('/[^A-Za-z0-9\  ]/', '', $youtube->title);
+  $id=$youtube->id;
+  $duration=$youtube->duration;
+  $channel=$youtube->channel;
+  $view=$youtube->view;
+
+  echo'<div class="menu2">
+  <table class="otable" width="100%">
+  <tbody>
+  <tr>
+  <td valign="middle" width="75px" align="center">
+  <div class="imahe">
+  <img src="https://ytimg.googleusercontent.com/vi/'.$id.'/mqdefault.jpg" style="border: 1px solid #222; border-radius: 2px; float: left;" width="95" height="60">
+  <h8><span>'.$duration.'</span></h8>
+  </div>
+  </td>
+  <td style="padding: 4px 6px 4px 6px;>
+  <span style=" font-size:="" 13px;"="">
+  <a href="/site_download.xhtml?get-id='.$id.'&name='.$title.'">'.htmlspecialchars($judul).'</a>
+  <br>
+  <span style="font-size: 11px;">
+  <i class="fa fa-eye" aria-hidden="true" style="color: #ccc;"></i> '.$view.'
+  <br>
+  <i class="fa fa-calendar" aria-hidden="true" style="color: #ddd;"></i> '.$channel.'
+  </span>
+  </td>
+  </tr>
+  </tbody>
+  </table>
+  </div>';
 }
 
 ?>
