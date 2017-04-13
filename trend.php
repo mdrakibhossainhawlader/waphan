95b529978f6a8d9774810ab69fcde92856d28ae8
pidewap <pidewap@gmail.com>
2017-04-14 00:44:10 +0700

    Update trend.php

--- trend.php
+++ trend.php
@@ -15,7 +15,7 @@ foreach ($json as $youtube) {
   $bukak="document.write('";
   $tutup="');";
 
-  echo ''.$bukak.'<div class="menu2"><table class="otable" width="100%"><tbody><tr><td valign="middle" width="75px" align="center"><div class="imahe"><img src="https://ytimg.googleusercontent.com/vi/'.$id.'/mqdefault.jpg" style="border: 1px solid #222; border-radius: 2px; float: left;" width="95" height="60"><h8><span>'.$duration.'</span></h8></div></td><td style="padding: 4px 6px 4px 6px;><span style="font-size: 13px;"><a href="/site_download.xhtml?get-id='.$id.'&get-name='.$title.'">'.htmlspecialchars($title).'</a></span><br/><span style="font-size: 11px;"><i class="fa fa-eye" aria-hidden="true" style="color: #ccc;"></i> '.$view.'<br/><i class="fa fa-calendar" aria-hidden="true" style="color: #ddd;"></i> '.$channel.'</span></td></tr></tbody></table></div>'.$tutup.'
+  echo ''.$bukak.'<div class="fl odd"><a href="/site_download.xhtml?get-id='.$id.'&get-name='.$title.'"><div><div><img src="http://ytimg.googleusercontent.com/vi/'.$id.'/default.jpg" width="90" height="60" alt=""></div><div>'.$title.'<br><span><i><small>'.$duration.' </small></i></span></div></div></a></div>'.$tutup.'
   
   ';
 }
